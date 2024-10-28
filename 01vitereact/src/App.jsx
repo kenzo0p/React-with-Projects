3735c8c8a82663f02044430f8ec3ea8d1238498b
Om Bhor <ombhor617@gmail.com>
2024-10-28 14:35:01 +0530

    creating own library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,15 @@
 import Chai from './chai';
 function App() {
+  const username = "chai aur code"
 
 
   return (
-  //  <h1>hello world | om bhor,  this is react with vite</h1>
-  <>
-  <Chai/>
-  <h1>hii i am ready</h1>
-  </>
+    //<h1>hello world | om bhor,  this is react with vite</h1>
+    <>
+      <Chai />
+      <h1>chai aur react {username}</h1>
+      <h1>hii i am ready</h1>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,47 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import { jsx as _jsx} from "react/jsx-runtime.js"
+import React from 'react'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>custom app</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+// const AnotherElement = (
+//   <a href="https://google.com" target='_blank'>google</a>
+// )
+
+const anotherUser = "chai aur react";
+
+const ReactElement = React.createElement( //bable inject the this method create element
+  'a' ,
+  {
+    href:'https://google.com' , target:'_blank'
+  },
+  'click me to visit google',
+  anotherUser
+)
+
+createRoot(document.getElementById('root')).
+render(
+  // MyApp()
+
+  // <MyApp/>
+  // <App/>
+  ReactElement
+  
 )
