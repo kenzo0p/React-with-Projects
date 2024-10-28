477b31231120ac4524f4ee3b80c4ec35ccbf004a
Om Bhor <ombhor617@gmail.com>
2024-10-28 12:04:58 +0530

    right practices

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,13 @@
-
+import Chai from './chai';
 function App() {
 
 
   return (
-   <h1>hello world | om bhor,  this is react with vite</h1>
+  //  <h1>hello world | om bhor,  this is react with vite</h1>
+  <>
+  <Chai/>
+  <h1>hii i am ready</h1>
+  </>
   )
 }
 
