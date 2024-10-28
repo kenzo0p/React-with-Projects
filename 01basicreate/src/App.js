12ceca2dd7a59007e10ada91eb2c5b24d2d13aa5
Om Bhor <ombhor617@gmail.com>
2024-10-28 11:22:48 +0530

    1st lesson with hello world

---
+++ 01basicreate/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <h1>Hello World | Om Bhor , in this lecture I learn about how  create react app using react and vite , for we create a first basic project that i am writing now 
+      after that we create a create react app using vite using command npm create vite@latest and for first folder npx create-react-app 01basicreate
+      after that for making project with react we go to react folder and did some changes and after we write commans npm run start and yes its start
+    </h1>
+  )
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+   <h1>hello world | om bhor,  this is react with vite</h1>
+  )
+}
+
+export default App
