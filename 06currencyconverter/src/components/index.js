a8e178157287c8518cae5c882958c03ea26a8c2c
Om Bhor <ombhor617@gmail.com>
2024-10-30 22:39:49 +0530

    currency converter

---
+++ 06currencyconverter/src/components/index.js
@@ -0,0 +1,9 @@
+import InputBox from "./InputBox";
+
+
+export {InputBox};
+
+
+
+
+// this file we used here fo that if we create more components strore in this file and just export this file app.js if you have so many components
